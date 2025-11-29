899b20e1e59d6e37331e6e27dcfec1a7e66457f6
lhdht <15229684090@163.com>
2025-11-29 11:20:47 +0800

    feature: define const

---
+++ pkg/constant/header.go
@@ -0,0 +1,17 @@
+package constant
+
+const (
+	XRealIp           = "x-real-ip"
+	Authorization     = "authorization"
+	AuthorizationType = "authorization-type"
+
+	FFTraceId  = "ff-trace-id"
+	FFSession  = "ff-session"
+	FFUserId   = "ff-user-id"
+	FFAppId    = "ff-app-id"
+	FFTenantId = "ff-tenant-id"
+	FFClientIp = "ff-client-ip"
+
+	GrpcGatewayAuth = "grpc-gateway-auth"
+	HttpGatewayAuth = "http-gateway-auth"
+)
