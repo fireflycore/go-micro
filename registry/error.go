8f11ad8367ff03d3e00083c2ddc8a4df781d8730
lhdht <15229684090@163.com>
2026-01-04 15:17:43 +0800

    feature: define registry error

--- registry/error.go
+++ registry/error.go
@@ -11,4 +11,13 @@ var (
 	ErrServiceMethodNotExists = errors.New("服务方法不存在")
 	// ErrServiceNodeMethodNotExists 表示服务节点不包含指定方法。
 	ErrServiceNodeMethodNotExists = errors.New("服务节点不包含该方法")
+
+	// ErrServiceConfigIsNil 表示服务配置为空。
+	ErrServiceConfigIsNil = errors.New("服务配置为空")
+	// ErrServiceNodeIsNil 表示服务节点对象为空。
+	ErrServiceNodeIsNil = errors.New("服务节点对象为空")
+	// ErrMetaAppIdIsEmpty 表示元数据 AppId 为空。
+	ErrMetaAppIdIsEmpty = errors.New("元数据 AppId 为空")
+	// ErrMetaEnvIsEmpty 表示元数据 Env 为空。
+	ErrMetaEnvIsEmpty = errors.New("元数据 Env 为空")
 )
