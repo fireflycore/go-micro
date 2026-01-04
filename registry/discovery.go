2a00a7b47564b5c5e71658fc0026d2bd8bff8834
lhdht <15229684090@163.com>
2026-01-04 15:15:55 +0800

    feature: registry Discovery

--- registry/discovery.go
+++ registry/discovery.go
@@ -20,14 +20,3 @@ func (s ServiceDiscover) GetNodes(appId string) ([]*ServiceNode, error) {
 	}
 	return nil, ErrServiceNodeNotExists
 }
-
-// ServiceMethods 服务方法映射（method -> appId）。
-type ServiceMethods map[string]string
-
-// GetAppId 根据 gRPC 方法名返回归属的 appId。
-func (s ServiceMethods) GetAppId(sm string) (string, error) {
-	if v, ok := s[sm]; ok {
-		return v, nil
-	}
-	return "", ErrServiceMethodNotExists
-}
