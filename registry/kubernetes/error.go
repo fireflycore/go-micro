edfacfb1550545c38f007d28a27d0fc09216b301
lhdht <15229684090@163.com>
2026-01-05 17:39:22 +0800

    fix: abandoned k8s realize method

--- registry/kubernetes/error.go
+++
@@ -1,7 +0,0 @@
-package kubernetes
-
-// ErrConfigFailedFormat 表示获取 K8s 配置失败的错误格式。
-const ErrConfigFailedFormat = "获取 k8s 配置失败: %v"
-
-// ErrClientFailedFormat 表示创建 K8s 客户端失败的错误格式。
-const ErrClientFailedFormat = "创建 k8s 客户端失败: %v"
