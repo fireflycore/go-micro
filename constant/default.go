b674cea9606c3eb51b19c197f9c6e18faf12ef43
lhdht <15229684090@163.com>
2026-01-06 10:07:04 +0800

    feature: define default value

---
+++ constant/default.go
@@ -0,0 +1,12 @@
+package constant
+
+const (
+	// DefaultNamespace 默认命名空间
+	DefaultNamespace = "firefly"
+	// DefaultMaxRetry 默认重试次数
+	DefaultMaxRetry = 3
+	// DefaultTTL 默认心跳间隔
+	DefaultTTL = 10
+	// DefaultVersion 默认版本号
+	DefaultVersion = "v0.0.1"
+)
