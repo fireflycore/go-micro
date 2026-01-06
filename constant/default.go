cebcba688fea567cb6440f417b4257cbd3dd2ac1
lhdht <15229684090@163.com>
2026-01-06 10:15:21 +0800

    feature: define default

--- constant/default.go
+++ constant/default.go
@@ -3,6 +3,10 @@ package constant
 const (
 	// DefaultNamespace 默认命名空间
 	DefaultNamespace = "firefly"
+	// DefaultNetworkSN 默认网卡命名
+	DefaultNetworkSN = "firefly-main-network"
+	// DefaultExternalNetworkAddress 默认外网地址
+	DefaultExternalNetworkAddress = "127.0.0.1"
 	// DefaultMaxRetry 默认重试次数
 	DefaultMaxRetry = 3
 	// DefaultTTL 默认心跳间隔
