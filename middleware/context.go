94cd189eca5938473429d6b457506759a4c271b6
lhdht <15229684090@163.com>
2026-02-09 17:12:57 +0800

    fix: now set invoke service status

--- middleware/context.go
+++ middleware/context.go
@@ -4,6 +4,7 @@ package middleware
 import (
 	"context"
 
+	"github.com/fireflycore/go-micro/constant"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
@@ -11,6 +12,7 @@ import (
 // PropagateIncomingMetadata 将入站元数据传播到出站上下文中
 func PropagateIncomingMetadata(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	md, _ := metadata.FromIncomingContext(ctx)
+	md.Set(constant.InvokeService, constant.InvokeServiceStart)
 	// 复制一份元数据到 OutgoingContext，保证服务内的下游 gRPC 调用能自动携带同一套上下文信息，
 	// 同时避免对原始入站元数据的意外修改。
 	oc := metadata.NewOutgoingContext(ctx, md.Copy())
