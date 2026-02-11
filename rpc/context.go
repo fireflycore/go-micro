de2f6744191f87384799f6ee96b3fb2c9fb6e5e9
lhdht <15229684090@163.com>
2026-02-11 16:11:27 +0800

    feature: rpc context func

---
+++ rpc/context.go
@@ -0,0 +1,30 @@
+package rpc
+
+import (
+	"context"
+	"github.com/fireflycore/go-micro/constant"
+	"google.golang.org/grpc/metadata"
+)
+
+// SetRemoteInvokeServiceBeforeContext 设置远程调用前置上下文
+func SetRemoteInvokeServiceBeforeContext(ctx context.Context, token, appId, endpoint string) context.Context {
+	pm, _ := metadata.FromIncomingContext(ctx)
+
+	md := pm.Copy()
+	md.Set(constant.InvokeServiceAuth, token)
+	md.Set(constant.InvokeServiceAppId, appId)
+	md.Set(constant.InvokeServiceEndpoint, endpoint)
+
+	return metadata.NewOutgoingContext(ctx, md)
+}
+
+// SetRemoteInvokeServiceAfterContext 设置远程调用后置上下文
+func SetRemoteInvokeServiceAfterContext(ctx context.Context, appId, clientIp string) context.Context {
+	pm, _ := metadata.FromIncomingContext(ctx)
+
+	md := pm.Copy()
+	md.Set(constant.TargetServiceAppId, appId)
+	md.Set(constant.TargetServiceEndpoint, clientIp)
+
+	return metadata.NewOutgoingContext(ctx, md)
+}
