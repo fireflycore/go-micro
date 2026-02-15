c9b9ed0a3f3cf9cf12906786b62497a1dfd3d825
lhdht <15229684090@163.com>
2026-02-15 09:50:00 +0800

    define: client type

---
+++ constant/client.go
@@ -0,0 +1,17 @@
+package constant
+
+const (
+	ClientTypeUnspecified uint32 = 0
+	// 网页端浏览器
+	ClientTypeWebBrowser uint32 = 1
+	// 小程序
+	ClientTypeMiniProgram uint32 = 2
+	// 移动端应用
+	ClientTypeMobileApp uint32 = 3
+	// 桌面端应用
+	ClientTypeDesktopApp uint32 = 4
+	// 服务端
+	ClientTypeServer uint32 = 5
+	// 嵌入式
+	ClientTypeEmbedded uint32 = 6
+)
