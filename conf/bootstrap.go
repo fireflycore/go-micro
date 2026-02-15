13c5d391809c0526f8b4c037d6008257aa7efb58
lhdht <15229684090@163.com>
2026-02-15 10:36:53 +0800

    feature: define system info

--- conf/bootstrap.go
+++ conf/bootstrap.go
@@ -4,4 +4,12 @@ type BootstrapConf interface {
 	GetAppId() string
 	GetServiceEndpoint() string
 	GetServiceAuthToken() string
+
+	GetClientName() string
+	GetClientType() uint32
+	GetClientVersion() string
+
+	GetSystemName() string
+	GetSystemType() uint32
+	GetSystemVersion() string
 }
