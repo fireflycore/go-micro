361718e8a2e174712f3018dfad4e63255d77e7d0
lhdht <15229684090@163.com>
2026-02-15 11:48:35 +0800

    fix: amend

--- constant/client.go
+++ constant/client.go
@@ -2,16 +2,16 @@ package constant
 
 const (
 	ClientTypeUnspecified uint32 = 0
-	// 网页端浏览器
+	// ClientTypeWebBrowser 网页端浏览器
 	ClientTypeWebBrowser uint32 = 1
-	// 小程序
+	// ClientTypeMiniProgram 小程序
 	ClientTypeMiniProgram uint32 = 2
-	// 移动端应用
+	// ClientTypeMobileApp 移动端应用
 	ClientTypeMobileApp uint32 = 3
-	// 桌面端应用
+	// ClientTypeDesktopApp 桌面端应用
 	ClientTypeDesktopApp uint32 = 4
-	// 服务端
+	// ClientTypeServer 服务端
 	ClientTypeServer uint32 = 5
-	// 嵌入式
+	// ClientTypeEmbedded 嵌入式
 	ClientTypeEmbedded uint32 = 6
 )
--- constant/system.go
+++ constant/system.go
@@ -1,9 +1,10 @@
 package constant
 
 const (
-	SystemTypeWindows uint32 = 1
-	SystemTypeMacOS   uint32 = 2
-	SystemTypeLinux   uint32 = 3
-	SystemTypeAndroid uint32 = 4
-	SystemTypeIOS     uint32 = 5
+	SystemTypeUnspecified uint32 = 0
+	SystemTypeWindows     uint32 = 1
+	SystemTypeMacOS       uint32 = 2
+	SystemTypeLinux       uint32 = 3
+	SystemTypeAndroid     uint32 = 4
+	SystemTypeIOS         uint32 = 5
 )
