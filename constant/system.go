aa4ac99feb937aff4370ca8a91b458c72485e126
lhdht <15229684090@163.com>
2026-02-16 17:20:22 +0800

    feature: add new os

--- constant/system.go
+++ constant/system.go
@@ -3,8 +3,11 @@ package constant
 const (
 	SystemTypeUnspecified uint32 = 0
 	SystemTypeWindows     uint32 = 1
-	SystemTypeMacOS       uint32 = 2
+	SystemTypeMacOS       uint32 = 3
 	SystemTypeLinux       uint32 = 3
 	SystemTypeAndroid     uint32 = 4
 	SystemTypeIOS         uint32 = 5
+	SystemTypeHarmony     uint32 = 6
+	SystemTypeChromeOS    uint32 = 7
+	SystemTypeFreeBSD     uint32 = 8
 )
