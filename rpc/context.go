ebea2ca5032cecb1e50de7a14eb8605bfcb8da79
lhdht <15229684090@163.com>
2026-02-27 22:16:25 +0800

    fix: simple logic

--- rpc/context.go
+++ rpc/context.go
@@ -6,28 +6,12 @@ import (
 
 	"github.com/fireflycore/go-micro/conf"
 	"github.com/fireflycore/go-micro/constant"
-	"github.com/google/uuid"
 	"google.golang.org/grpc/metadata"
 )
 
 // NewRemoteInvokeServiceContext 初始化远程调用服务上下文, 适用于服务自身（无其他用户特征）调用其他服务时，可基于此上下文进行设置上下文
 func NewRemoteInvokeServiceContext(bootstrapConf conf.BootstrapConf) context.Context {
-	md := metadata.Pairs()
-
-	md.Set(constant.RouteMethod, constant.RouteMethodService)
-	md.Set(constant.InvokeServiceAppId, bootstrapConf.GetAppId())
-	md.Set(constant.InvokeServiceEndpoint, bootstrapConf.GetServiceEndpoint())
-	md.Set(constant.InvokeServiceAuth, bootstrapConf.GetServiceAuthToken())
-
-	md.Set(constant.ClientType, fmt.Sprint(constant.ClientTypeServer))
-	md.Set(constant.ClientName, bootstrapConf.GetAppName())
-	md.Set(constant.ClientVersion, bootstrapConf.GetAppVersion())
-
-	md.Set(constant.SystemType, fmt.Sprint(bootstrapConf.GetSystemType()))
-	md.Set(constant.SystemName, bootstrapConf.GetSystemName())
-	md.Set(constant.SystemVersion, bootstrapConf.GetSystemVersion())
-
-	return metadata.NewOutgoingContext(context.Background(), md)
+	return SetRemoteInvokeServiceBeforeContext(context.Background(), bootstrapConf)
 }
 
 // SetRemoteInvokeServiceBeforeContext 设置远程调用前置上下文，基于当前上下文进行设置服务自身的信息，并设置链路追踪，一般是将其封装成中间件使用
@@ -47,14 +31,6 @@ func SetRemoteInvokeServiceBeforeContext(ctx context.Context, bootstrapConf conf
 	md.Set(constant.SystemType, fmt.Sprint(bootstrapConf.GetSystemType()))
 	md.Set(constant.SystemName, bootstrapConf.GetSystemName())
 	md.Set(constant.SystemVersion, bootstrapConf.GetSystemVersion())
-
-	if _, err := ParseMetaKey(md, constant.TraceId); err != nil {
-		md.Set(constant.TraceId, uuid.Must(uuid.NewV7()).String())
-	}
-	if spanId, err := ParseMetaKey(md, constant.SpanId); err == nil {
-		md.Set(constant.ParentId, spanId)
-	}
-	md.Set(constant.SpanId, uuid.Must(uuid.NewV7()).String())
 
 	return metadata.NewOutgoingContext(ctx, md)
 }
