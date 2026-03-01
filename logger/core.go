6cd5281f6429fa00d70c105b3f3f46bf97edec50
lhdht <15229684090@163.com>
2026-03-01 09:44:45 +0800

    fix: amend

--- logger/core.go
+++ logger/core.go
@@ -2,6 +2,8 @@ package logger
 
 import (
 	"context"
+	"github.com/fireflycore/go-micro/rpc"
+	"google.golang.org/grpc/metadata"
 
 	"github.com/fireflycore/go-micro/constant"
 	"go.uber.org/zap"
@@ -37,19 +39,21 @@ func (l *Core) WithError(ctx context.Context, msg string, fields ...zap.Field) {
 }
 
 func (l *Core) withContext(ctx context.Context, fields []zap.Field) []zap.Field {
-	if traceId, ok := ctx.Value(constant.TraceId).(string); ok {
+	md, _ := metadata.FromIncomingContext(ctx)
+
+	if traceId, err := rpc.ParseMetaKey(md, constant.TraceId); err == nil {
 		fields = append(fields, zap.String("trace_id", traceId))
 	}
-	if spanId, ok := ctx.Value(constant.SpanId).(string); ok {
+	if spanId, err := rpc.ParseMetaKey(md, constant.SpanId); err == nil {
 		fields = append(fields, zap.String("parent_id", spanId))
 	}
-	if userId, ok := ctx.Value(constant.UserId).(string); ok {
+	if userId, err := rpc.ParseMetaKey(md, constant.UserId); err == nil {
 		fields = append(fields, zap.String("user_id", userId))
 	}
-	if appId, ok := ctx.Value(constant.AppId).(string); ok {
+	if appId, err := rpc.ParseMetaKey(md, constant.AppId); err == nil {
 		fields = append(fields, zap.String("app_id", appId))
 	}
-	if tenantId, ok := ctx.Value(constant.TenantId).(string); ok {
+	if tenantId, err := rpc.ParseMetaKey(md, constant.TenantId); err == nil {
 		fields = append(fields, zap.String("tenant_id", tenantId))
 	}
 
