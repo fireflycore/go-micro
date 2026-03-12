15c69f2571d57315436839253b58797db2cd0da5
lhdht <15229684090@163.com>
2026-03-12 21:35:25 +0800

    fix: inject otel

--- rpc/client.go
+++ rpc/client.go
@@ -2,10 +2,15 @@ package rpc
 
 import (
 	"github.com/fireflycore/go-micro/conf"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func NewRemoteServiceGrpcClient(bootstrapConf conf.BootstrapConf) (*grpc.ClientConn, error) {
-	return grpc.NewClient(bootstrapConf.GetGatewayEndpoint(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+func NewGrpcClient(bootstrapConf conf.BootstrapConf) (*grpc.ClientConn, error) {
+	return grpc.NewClient(
+		bootstrapConf.GetGatewayEndpoint(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+	)
 }
