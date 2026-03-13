86f2fd2d2014f5a05d83417152d5e9aae720b9fb
lhdht <15229684090@163.com>
2026-03-13 17:36:43 +0800

    fix: 实现相关接口

--- telemetry/conf.go
+++ telemetry/conf.go
@@ -1,13 +1,30 @@
 package telemetry
 
 type Conf struct {
-	ServiceName    string
-	ServiceVersion string
-
 	OTLPEndpoint string
 	Insecure     bool
 
 	Traces  bool
 	Logs    bool
 	Metrics bool
+}
+
+func (c *Conf) GetOTLPEndpoint() string {
+	return c.OTLPEndpoint
+}
+
+func (c *Conf) GetOtelInsecure() bool {
+	return c.Insecure
+}
+
+func (c *Conf) GetOtelTraces() bool {
+	return c.Traces
+}
+
+func (c *Conf) GetOtelMetrics() bool {
+	return c.Metrics
+}
+
+func (c *Conf) GetOtelLogs() bool {
+	return c.Logs
 }
