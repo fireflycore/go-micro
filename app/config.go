e86a7ee01c9bc279ad15830f5f97e7516de746fb
lhdht <15229684090@163.com>
2026-04-27 21:02:32 +0800

    feature: app config

---
+++ app/config.go
@@ -0,0 +1,14 @@
+package app
+
+type Config struct {
+	// 应用id
+	Id string `json:"id"`
+	// 应用环境
+	Env string `json:"env"`
+	// 应用名称
+	Name string `json:"name"`
+	// 应用密钥
+	Secret string `json:"secret"`
+	// 应用版本
+	Version string `json:"version"`
+}
