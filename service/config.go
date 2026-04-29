2eb4ad876ee8d6d954aeb15e2504c5cb1b9274f6
lhdht <15229684090@163.com>
2026-04-29 20:57:04 +0800

    fix: amend

--- service/config.go
+++ service/config.go
@@ -1,17 +1,16 @@
 package service
 
 type Config struct {
-	// Service 表示业务服务名，例如 auth。
-	Service string `json:"service"`
+	// Name 表示业务服务名，例如 auth。
+	Name string `json:"name"`
+	// Type 表示服务类型片段，默认值通常为 svc。
+	Type string `json:"type"`
 	// Namespace 表示命名空间，例如 default。
 	Namespace string `json:"namespace"`
-	// ServiceType 表示服务类型片段，默认值通常为 svc。
-	ServiceType string `json:"service_type"`
 	// ClusterDomain 表示集群域，默认值通常为 cluster.local。
 	ClusterDomain string `json:"cluster_domain"`
 	// Port 表示业务服务监听端口，默认值通常为 9090。
-	Port uint16 `json:"port"`
-
+	Port uint `json:"port"`
 	// Weight 表示权重，默认值通常为 100。
 	Weight uint `json:"weight"`
 }
