64f2a2a63e35b7b9f4a7d0a8f776c2ff8b21d2a9
lhdht <15229684090@163.com>
2026-06-01 22:36:59 +0800

    feature: jws

---
+++ constant/jws.go
@@ -0,0 +1,8 @@
+package constant
+
+const (
+	// JWSAlgorithmEdDSA 表示 Firefly compact JWS 当前唯一允许的签名算法。
+	JWSAlgorithmEdDSA = "EdDSA"
+	// JWSTypeJWT 表示 Firefly compact JWS header typ 的标准值。
+	JWSTypeJWT = "JWT"
+)
